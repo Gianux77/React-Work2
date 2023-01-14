0d9df331f7f52f63d3a2c7789d60d86a566f860c
Gianux77 <gfg4899@gmail.com>
2023-01-14 11:53:13 -0300

    apis-v01

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+/*
+
+1. Instalar axios
+2. Importar axios
+3. Estado donde se almacena la respuesta
+4. useEffect con arreglo de dependencias
+5. Ejecutar axios
+6. Guardar la respuesta en el estado
+
+*/
+  return (
+    <div className="App">
+
+    </div>
+  )
+}
+
+export default App
