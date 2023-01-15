6489cd3b768d4aa5c054cb7d75305ed31ca533c4
Gianux77 <gfg4899@gmail.com>
2023-01-15 14:54:04 -0300

    sema3-apis-main

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import axios from 'react'
+import Beverages from './components/Beverages'
 
 function App() {
-  // const [count, setCount] = useState(0)
 /*
 
 1. Instalar axios
@@ -13,9 +14,31 @@ function App() {
 6. Guardar la respuesta en el estado
 
 */
+const [ dataBeverages, setDataBeverages ] = useState( [] )
+const [ name, setName ] = useState("margarita")
+
+  const getData = ()=>{
+  axios
+  .get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita`)
+  .then( resp =>  console.log(resp.drinks ))
+  .catch( error => console.error(error))
+  }
+
+  useEffect(()=>{
+    getData ();
+  },[])
+
   return (
     <div className="App">
-
+      <form>                                                        {/* onSubmit={(e)=> searchCharacter(e)} */}                                                            
+        <input type="text" placeholder="Buscar por nombre" />
+        <button type="submit">Buscar</button>
+      </form><br />
+      <hr />
+      <div className='beverages_app'>
+      <Beverages/>
+      </div>
+    
     </div>
   )
 }
---
+++ src/components/Beverages.jsx
@@ -0,0 +1,16 @@
+
+
+const Beverages = () =>{
+
+
+    return(
+        <div className="beverages-card">
+            <img src="" alt="" />
+            <h2>Nombre</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, 
+                enim tenetur quia necessitatibus quidem officia consectetur,</p>
+        </div>
+    )
+}
+
+export default Beverages
